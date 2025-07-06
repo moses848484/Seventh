d28f689b7f6e4c0f62d3ef67496a9c249a59a0a2
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-07 01:25:49 +0200

    new

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,67 +1,81 @@
-<header class="header_section">
-  <div class="container3">
-    <nav class="navbar navbar-expand-sm custom_nav-container">
-      <!-- Logo -->
-      <a href="/" class="navbar-brand">
-        <img src="images/sda3.png" class="sda_logo8" alt="Dashboard Logo">
-      </a>
+<!DOCTYPE html>
+<html lang="en">
 
-      <!-- Centered text (only visible on certain breakpoints if styled that way) -->
-      <span class="xs d-block text-center mx-auto">UNIVERSITY SDA.CHURCH</span>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Include your CSS files here -->
+</head>
 
-      <!-- Hamburger menu toggle (mobile) -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<body>
 
-      <!-- Collapsible menu -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="https://www.facebook.com/@universityadventist/">Attend Online</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Media</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Who We Are</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Give</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Locations</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Worship</a>
-          </li>
+  <header class="header_section">
+    <div class="container3">
+      <nav class="navbar navbar-expand-sm custom_nav-container">
+        <!-- Logo -->
+        <a href="/" class="navbar-brand">
+          <img src="images/sda3.png" class="sda_logo8" alt="Dashboard Logo">
+        </a>
 
-          <!-- Authentication Section -->
-          @if (Route::has('login'))
-            @auth
-              <!-- Desktop View -->
-              <li class="nav-item d-none d-md-block">
-                <a class="nav-link" href="/redirect">Go To Dashboard</a>
-              </li>
+        <!-- Centered text -->
+        <span class="xs d-block text-center mx-auto">UNIVERSITY SDA.CHURCH</span>
 
-              <!-- Mobile View -->
-              <li class="nav-item d-md-none text-center w-100">
-                <a class="nav-link" href="/redirect">Go To Dashboard</a>
-              </li>
-            @else
-              <li class="nav-item d-flex align-items-center">
-                <a class="btn btn-primary mr-2" id="logincss" href="{{ url('/redirect') }}">
-                  <i class="fas fa-user-circle"></i> Log In
-                </a>
-                <a class="btn btn-success" href="{{ route('register') }}">
-                  <i class="fas fa-user-plus"></i> Register
-                </a>
-              </li>
-            @endauth
-          @endif
-        </ul>
-      </div>
-    </nav>
-  </div>
-</header>
+        <!-- Hamburger menu toggle -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Collapsible menu -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="https://www.facebook.com/@universityadventist/">Attend Online</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Media</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Who We Are</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Give</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Locations</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Worship</a>
+            </li>
+
+            <!-- Authentication Section -->
+            @if (Route::has('login'))
+              @auth
+                <li class="nav-item d-none d-md-block">
+                  <a class="nav-link" href="/redirect">Go To Dashboard</a>
+                </li>
+                <li class="nav-item d-md-none text-center w-100">
+                  <a class="nav-link" href="/redirect">Go To Dashboard</a>
+                </li>
+              @else
+                <li class="nav-item d-flex align-items-center">
+                  <a class="btn btn-primary mr-2" id="logincss" href="{{ url('/redirect') }}">
+                    <i class="fas fa-user-circle"></i> Log In
+                  </a>
+                  <a class="btn btn-success" href="{{ route('register') }}">
+                    <i class="fas fa-user-plus"></i> Register
+                  </a>
+                </li>
+              @endauth
+            @endif
+
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+</body>
+
+</html>
