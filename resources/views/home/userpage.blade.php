fe4022882fd5459fd2308eca5680d185ee14b879
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-07 00:05:38 +0200

    new

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -21,7 +21,8 @@
    <link href="https://seventh-production.up.railway.app/home/css/style.css" rel="stylesheet" />
    <!-- responsive style -->
    <link href="https://seventh-production.up.railway.app/home/css/responsive.css" rel="stylesheet" />
-   <link rel="stylesheet" href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
    @notifyCss
 </head>
 
@@ -57,6 +58,7 @@
    <script src="https://seventh-production.up.railway.app/home/js/bootstrap.js"></script>
    <!-- custom js -->
    <script src="https://seventh-production.up.railway.app/home/js/custom.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
    <x-notify::notify />
    @notifyJs
