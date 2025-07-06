43c08fcb609b6d99800ac5f9f5be6c6adb62c15f
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-06 07:03:47 +0200

    Update userpage.blade.php

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -11,66 +11,52 @@
    <meta name="keywords" content="" />
    <meta name="description" content="" />
    <meta name="author" content="" />
-   <link rel="shortcut icon" href="{{ asset('images/sda3.png') }}" type="">
-
+   <link rel="shortcut icon" href="images/sda3.png" type="">
    <title>SDA Home Page</title>
-
-   <!-- Bootstrap core CSS -->
-   <link rel="stylesheet" href="{{ asset('..home/css/bootstrap.css') }}">
-
-   <!-- Font Awesome style -->
-   <link rel="stylesheet" href="{{ asset('...home/css/font-awesome.min.css') }}">
-
+   <!-- bootstrap core css -->
+   <link rel="stylesheet" type="text/css" href=".home/css/bootstrap.css" />
+   <!-- font awesome style -->
+   <link href=".home/css/font-awesome.min.css" rel="stylesheet" />
    <!-- Custom styles for this template -->
-   <link rel="stylesheet" href="{{ asset('...home/css/style.css') }}">
-
-   <!-- Responsive style -->
-   <link rel="stylesheet" href="{{ asset('...home/css/responsive.css') }}">
-
-   <!-- FontAwesome 6.5.2 -->
-   <link rel="stylesheet" href="{{ asset('css/fontawesome-free-6.5.2-web/css/all.min.css') }}" type="text/css">
-
+   <link href=".home/css/style.css" rel="stylesheet" />
+   <!-- responsive style -->
+   <link href=".home/css/responsive.css" rel="stylesheet" />
+   <link rel="stylesheet" href="<?php echo asset('css/fontawesome-free-6.5.2-web/css/all.min.css') ?>" type="text/css">
    @notifyCss
 </head>
 
 <body>
 
    <div class="hero_area">
-      <!-- header section starts -->
+      <!-- header section strats -->
       @include('home.header')
       <!-- end header section -->
-
       <!-- slider section -->
       @include('home.slider')
       <!-- end slider section -->
    </div>
-
    <!-- containers section -->
    @include('home.containers')
-
-   <!-- prayer section -->
+   <!-- end containers section -->
+   <!-- prayersection -->
    @include('home.prayer')
-
-   <!-- welcome section -->
+   <!-- end prayer section -->
    @include('home.welcome')
-
    <!-- songs section -->
    @include('home.songs')
-
-   <!-- footer section -->
+   <!-- end songs section -->
+   <!-- footer start -->
    @include('home.footer')
+   <!-- footer end -->
 
-   <!-- jQuery -->
-   <script src="{{ asset('home/js/jquery-3.4.1.min.js') }}"></script>
-
-   <!-- Popper JS -->
-   <script src="{{ asset('home/js/popper.min.js') }}"></script>
-
-   <!-- Bootstrap JS -->
-   <script src="{{ asset('home/js/bootstrap.js') }}"></script>
-
-   <!-- Custom JS -->
-   <script src="{{ asset('home/js/custom.js') }}"></script>
+   <!-- jQery -->
+   <script src="home/js/jquery-3.4.1.min.js"></script>
+   <!-- popper js -->
+   <script src="home/js/popper.min.js"></script>
+   <!-- bootstrap js -->
+   <script src="home/js/bootstrap.js"></script>
+   <!-- custom js -->
+   <script src="home/js/custom.js"></script>
 
    <x-notify::notify />
    @notifyJs
