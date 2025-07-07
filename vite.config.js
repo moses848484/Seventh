4381ed3f2a64cbb2e114628d6926ee775b2a0f6d
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-07 03:02:13 +0200

    qw

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,15 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
   plugins: [
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
+      input: ["resources/sass/app.scss", "resources/js/app.js"],
       refresh: true,
     }),
   ],
-
   build: {
-    outDir: 'public/build',   // this should be here or default
+    outDir: "public/build",
     manifest: true,
     rollupOptions: {
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ["resources/sass/app.scss", "resources/js/app.js"],
     },
-   },  
+  },
 });
