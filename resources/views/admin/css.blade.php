011852487e70cc1ee6dba0b38202cf071a7e76c8
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-07 05:40:03 +0200

    save

--- resources/views/admin/css.blade.php
+++ resources/views/admin/css.blade.php
@@ -1,27 +1,48 @@
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>SDA Admin</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="admin/assets/css/fontawesome-free-6.5.2-web/css/all.min.css">
-    <link rel="stylesheet" href="admin/assets/css/fontawesome-free-6.6.0-web/css/all.min.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="admin/assets/vendors/jvectormap/jquery-jvectormap.css">
-    <link rel="stylesheet" href="admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
-    <link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="admin/assets/css/style.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="admin/assets/css/style1.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="admin/assets/images/sda3.png" />
-    <script src="/images/script.js"></script>
-    
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>SDA Admin</title>
+<!-- plugins:css -->
+<link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
+<link rel="stylesheet" href="admin/assets/css/fontawesome-free-6.5.2-web/css/all.min.css">
+<link rel="stylesheet" href="admin/assets/css/fontawesome-free-6.6.0-web/css/all.min.css">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href="admin/assets/vendors/jvectormap/jquery-jvectormap.css">
+<link rel="stylesheet" href="admin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+<link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.carousel.min.css">
+<link rel="stylesheet" href="admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
 
+<link rel="shortcut icon" href="https://seventh-production.up.railway.app/images/sda3.png" type="">
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="https://seventh-production.up.railway.app/home/css/bootstrap.css" />
+<!-- font awesome style -->
+<link href="https://seventh-production.up.railway.app/home/css/font-awesome.min.css" rel="stylesheet" />
+<!-- Custom styles for this template -->
+<link href="https://seventh-production.up.railway.app/home/css/style.css" rel="stylesheet" />
+<!-- responsive style -->
+<link href="https://seventh-production.up.railway.app/home/css/responsive.css" rel="stylesheet" />
+<link rel="stylesheet"
+    href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
+
+
+<!-- jQery -->
+<script src="https://seventh-production.up.railway.app/home/js/jquery-3.4.1.min.js"></script>
+<!-- popper js -->
+<script src="https://seventh-production.up.railway.app/home/js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="https://seventh-production.up.railway.app/home/js/bootstrap.js"></script>
+<!-- custom js -->
+<script src="https://seventh-production.up.railway.app/home/js/custom.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- endinject -->
+<!-- Layout styles -->
+<link rel="stylesheet" href="admin/assets/css/style.css">
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="admin/assets/css/style1.css">
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js">
+<!-- End layout styles -->
+<link rel="shortcut icon" href="admin/assets/images/sda3.png" />
+<script src="/images/script.js"></script>
--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -1,66 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-   <!-- Basic -->
-   <meta charset="utf-8" />
-   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-   <!-- Mobile Metas -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-   <!-- Site Metas -->
-   <meta name="keywords" content="" />
-   <meta name="description" content="" />
-   <meta name="author" content="" />
-   <link rel="shortcut icon" href="https://seventh-production.up.railway.app/images/sda3.png" type="">
-   <title>SDA Home Page</title>
-   <!-- bootstrap core css -->
-   <link rel="stylesheet" type="text/css" href="https://seventh-production.up.railway.app/home/css/bootstrap.css" />
-   <!-- font awesome style -->
-   <link href="https://seventh-production.up.railway.app/home/css/font-awesome.min.css" rel="stylesheet" />
-   <!-- Custom styles for this template -->
-   <link href="https://seventh-production.up.railway.app/home/css/style.css" rel="stylesheet" />
-   <!-- responsive style -->
-   <link href="https://seventh-production.up.railway.app/home/css/responsive.css" rel="stylesheet" />
-   <link rel="stylesheet" href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
    @notifyCss
+   <!-- Required meta tags -->
+   @include('admin.css')
 </head>
 
 <body>
    <!-- header section strats -->
-      @include('home.header')
-   <div class="hero_area">
-      
-      <!-- end header section -->
-      <!-- slider section -->
-      @include('home.slider')
-      <!-- end slider section -->
-   <!-- containers section -->
-   @include('home.containers')
-   <!-- end containers section -->
-   <!-- prayersection -->
-   @include('home.prayer')
-   <!-- end prayer section -->
-   @include('home.welcome')
-   <!-- songs section -->
-   @include('home.songs')
-   <!-- end songs section -->
-   <!-- footer start -->
-   @include('home.footer')
-   <!-- footer end -->
-
-   <!-- jQery -->
-   <script src="https://seventh-production.up.railway.app/home/js/jquery-3.4.1.min.js"></script>
-   <!-- popper js -->
-   <script src="https://seventh-production.up.railway.app/home/js/popper.min.js"></script>
-   <!-- bootstrap js -->
-   <script src="https://seventh-production.up.railway.app/home/js/bootstrap.js"></script>
-   <!-- custom js -->
-   <script src="https://seventh-production.up.railway.app/home/js/custom.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-   <x-notify::notify />
-   @notifyJs
-     </div>
+   <div class="container-scroller">
+      <div class="hero_area">
+         <!-- end header section -->
+         <!-- slider section -->
+         @include('home.slider')
+         <!-- end slider section -->
+         <!-- containers section -->
+         @include('home.containers')
+         <!-- end containers section -->
+         <!-- prayersection -->
+         @include('home.prayer')
+         <!-- end prayer section -->
+         @include('home.welcome')
+         <!-- songs section -->
+         @include('home.songs')
+         <!-- end songs section -->
+         <!-- footer start -->
+         @include('home.footer')
+         <!-- footer end -->
+         <x-notify::notify />
+         @notifyJs
+      </div>
 </body>
 
 </html>
