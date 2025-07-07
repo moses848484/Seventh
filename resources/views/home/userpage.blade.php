e9d98ab80ded417b663fc68c2851cc7ec98f6cb2
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-07 06:48:39 +0200

    fg

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -27,14 +27,14 @@
 </head>
 
 <body>
- <div class="hero_area"> 
+ <div class="hero_area">
+   <div class="container-scroller">
       <!-- header section strats -->
       @include('home.header')
       <!-- end header section -->
       <!-- slider section -->
       @include('home.slider')
       <!-- end slider section -->
-          </div>
          <!-- containers section -->
          @include('home.containers')
          <!-- end containers section -->
@@ -62,6 +62,7 @@
          <x-notify::notify />
          @notifyJs
       </div>
+   </div>
 </body>
 
 </html>
