d73094a4c59cfbbc066cccbd238dbf0fdd2c9fb8
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-08 02:04:53 +0200

    fgfg

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -8,7 +8,7 @@
             <span class="xs d-block text-center mx-auto">UNIVERSITY SDA.CHURCH</span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
