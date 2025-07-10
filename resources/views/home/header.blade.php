fdb99c97b388dfc0847e6fa978db230c92903247
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-10 13:25:48 +0200

    dcas

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,40 +1,42 @@
 <header class="header_section">
     <div class="container3">
-        <nav class="navbar navbar-expand-md custom_nav-container py-3 px-4">
-            <div class="d-flex align-items-center brand-wrapper">
+        <nav class="navbar navbar-expand-md custom_nav-container py-3 px-5">
+              <div class="d-flex align-items-center brand-wrapper">
                 <a href="/" class="navbar-brand mb-0">
                     <img src="images/sda3.png" class="sda_logo8" alt="Dashboard Logo">
                 </a>
-                <span class="xs">SDA.CHURCH</span>
+                <span class="xs d-block text-center mx-auto">SDA.CHURCH</span>
             </div>
-
-            <!-- Toggler for small screens -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <!-- Navigation links -->
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">ATTEND ONLINE</a>
+                        <a class="nav-link" href="https://www.facebook.com/@universityadventist/">ATTEND ONLINE <span
+                                class="sr-only">(current)</span></a>
                     </li>
+
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">MEDIA</a>
+                        <a class="nav-link" href="index.html">MEDIA <span class="sr-only">(current)</span></a>
                     </li>
+
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">WHO WE ARE</a>
+                        <a class="nav-link" href="index.html">WHO WE ARE &nbsp;<span
+                                class="sr-only">(current)</span></a>
                     </li>
+
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">GIVE</a>
+                        <a class="nav-link" href="index.html">GIVE &nbsp;<span class="sr-only">(current)</span></a>
                     </li>
+
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">LOCATIONS</a>
+                        <a class="nav-link" href="index.html">LOCATIONS&nbsp;<span class="sr-only">(current)</span></a>
                     </li>
+
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">WORSHIP</a>
+                        <a class="nav-link" href="index.html">WORSHIP &nbsp;<span class="sr-only">(current)</span></a>
                     </li>
 
 
