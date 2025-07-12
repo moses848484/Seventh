e1238455deeffa84d7fc937327b251e05c9edc04
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-12 22:02:37 +0200

    sdd

--- resources/views/admin/css1.blade.php
+++ resources/views/admin/css1.blade.php
@@ -2,14 +2,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>SDA Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="admin/assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="admin/assets/css/fontawesome-free-6.5.2-web/css/all.min.css">
+    <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/fontawesome-free-6.5.2-web/css/all.min.css">
     <!-- endinject -->
 
-    <link rel="stylesheet" href="admin/assets/css/style1.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style1.css">
+    <link rel="stylesheet" href="https://seventh-production.up.railway.app//node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://seventh-production.up.railway.app//node_modules/bootstrap/dist/js/bootstrap.bundle.min.js">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="admin/assets/images/sda3.png" />
+    <link rel="shortcut icon" href="https://seventh-production.up.railway.app/admin/assets/images/sda3.png" />
 
     <script src="/images/script.js"></script>
