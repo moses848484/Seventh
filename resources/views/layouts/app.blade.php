ad6ecd89191384033ae3b9318101bd7c7b11651c
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-14 14:45:16 +0200

    sdd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Tailwind CDN (for quick testing only) -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +15,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Styles -->
     @livewireStyles
 </head>
@@ -27,27 +24,27 @@
     <x-banner />
 
     <div class="">
-        <!-- Responsive Header with Livewire Navigation Menu -->
-        <header class="bg-transparent ">
-            <!-- Navigation Menu and Header Title -->
-            <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-
-                <!-- Left side: Page Heading (if any) -->
-                @if (isset($header))
-                    <div>
-                        {{ $header }}
-                    </div>
-                @endif
-
-                <!-- Right side: Livewire Navigation Menu -->
-                <div class="flex items-center justify-end">
-                    @livewire('navigation-menu') <!-- Livewire Navigation Menu positioned on the right -->
-                </div>
+   <!-- Responsive Header with Livewire Navigation Menu -->
+<header class="bg-transparent ">
+    <!-- Navigation Menu and Header Title -->
+    <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+        
+        <!-- Left side: Page Heading (if any) -->
+        @if (isset($header))
+            <div>
+                {{ $header }}
             </div>
-        </header>
+        @endif
+
+        <!-- Right side: Livewire Navigation Menu -->
+        <div class="flex items-center justify-end">
+            @livewire('navigation-menu') <!-- Livewire Navigation Menu positioned on the right -->
+        </div>
+    </div>
+</header>
         <!-- Page Content -->
         <main>
-            {{ $slot }}
+            {{ $slot }}    
         </main>
     </div>
 
@@ -55,6 +52,5 @@
 
     @livewireScripts
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
