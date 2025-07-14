d152ff1867990c89af50d24d97845c11970faa30
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-14 11:55:37 +0200

    fdfdf

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -186,6 +186,9 @@
                     </div>
                 </li>
                 <li class="nav-item" style="margin-top: 190px;">
+                    <x-app-layout class="bg-white">
+                        <x-notify::notify /> <!-- Position in mobile view -->
+                    </x-app-layout>
                 </li>
         </div>
     </nav>
