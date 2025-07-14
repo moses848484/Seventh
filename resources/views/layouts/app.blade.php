d8755049994d9ea84515287158b942022819c819
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-07-14 09:56:22 +0200

    hghg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Styles -->
@@ -24,27 +24,27 @@
     <x-banner />
 
     <div class="">
-   <!-- Responsive Header with Livewire Navigation Menu -->
-<header class="bg-transparent ">
-    <!-- Navigation Menu and Header Title -->
-    <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-        
-        <!-- Left side: Page Heading (if any) -->
-        @if (isset($header))
-            <div>
-                {{ $header }}
-            </div>
-        @endif
+        <!-- Responsive Header with Livewire Navigation Menu -->
+        <header class="bg-transparent ">
+            <!-- Navigation Menu and Header Title -->
+            <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
 
-        <!-- Right side: Livewire Navigation Menu -->
-        <div class="flex items-center justify-end">
-            @livewire('navigation-menu') <!-- Livewire Navigation Menu positioned on the right -->
-        </div>
-    </div>
-</header>
+                <!-- Left side: Page Heading (if any) -->
+                @if (isset($header))
+                    <div>
+                        {{ $header }}
+                    </div>
+                @endif
+
+                <!-- Right side: Livewire Navigation Menu -->
+                <div class="flex items-center justify-end">
+                    @livewire('navigation-menu') <!-- Livewire Navigation Menu positioned on the right -->
+                </div>
+            </div>
+        </header>
         <!-- Page Content -->
         <main>
-            {{ $slot }}    
+            {{ $slot }}
         </main>
     </div>
 
