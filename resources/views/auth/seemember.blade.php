d9c421990b79d0c36994bcd9aa072f38110bf810
moses848484 <moses.blake.simataa@gmail.com>
2025-07-15 23:06:05 +0200

    sdsd

--- resources/views/auth/seemember.blade.php
+++ resources/views/auth/seemember.blade.php
@@ -8,6 +8,7 @@
     <meta name="keywords" content="Church, Manager, Member registration, Donation, Tithe Manager" />
     <link rel="stylesheet" href="<?php echo asset('css/viewmember.css'); ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo asset('css/fontawesome-free-6.5.2-web/css/all.min.css'); ?>" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>View Church Members</title>
     <style>
         /* Custom Autofill input styling */
