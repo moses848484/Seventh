367f010a747bb6883d59f4d034f2d5e27e081ced
moses848484 <moses.blake.simataa@gmail.com>
2025-07-16 09:41:34 +0200

    sdsd

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,3 @@ export default defineConfig({
         }),
     ],
 });
-import Alpine from 'alpinejs'
-import focus from '@alpinejs/focus'
-
-Alpine.plugin(focus)
-window.Alpine = Alpine
-Alpine.start()
