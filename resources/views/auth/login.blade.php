05aee47fb1b7ed9072d868218157fa0c26be7dad
moses848484 <moses.blake.simataa@gmail.com>
2025-07-16 08:30:53 +0200

    dcdc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,86 +4,99 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Church Manager" />
-    <meta name="keywords" content="Church, Manager, Member registration, Donation, Tithe Manager" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/css/login.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" rel="stylesheet" />
+    <meta name="description" content="Church Manager">
+    <meta name="keywords" content="Church, Manager, Member registration, Donation, Tithe Manager">
 
-    <!----Name of page---->
+    <!-- Secure stylesheets -->
+    <link rel="stylesheet" href="{{ secure_asset('css/signup.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/fontawesome-free-6.5.2-web/css/all.min.css') }}">
 
-    <title>Login Page</title>
-    <title>Login</title>
+    <title>Sign Up Page</title>
+
+    <style>
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus,
+        input:-webkit-autofill:active {
+            -webkit-box-shadow: 0 0 0 30px white inset !important;
+            box-shadow: 0 0 0 30px white inset !important;
+            -webkit-text-fill-color: #04AA6D !important;
+        }
+    </style>
 </head>
 
 <body>
-
     <x-validation-errors class="mb-4" />
 
-    @session('status')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ $value }}
-        </div>
-    @endsession
-
-    <!--Login form-->
-    <div class="input-group">
-        <form method="POST" autocomplete="on" action="{{ route('login') }}">
+    <div id="register" class="form-container">
+        <form method="POST" action="{{ route('register') }}" autocomplete="on">
             @csrf
 
             <div class="logo">
-                <a class="navbar-brand brand-logo-mini" href="/"><img src="/images/sda.png" class="sda_logo"></a>
+                <img src="{{ secure_asset('images/sda.png') }}" alt="Logo" class="sda_logo">
+            </div>
 
-                <h2 class="login_word_on_form">Login</h2>
+            <h2 class="form-title">Sign up</h2>
+
+            <div class="input-row">
+                <!-- Name -->
                 <div class="input-group">
-                    <input id="email" name="email" :value="old('email')" type="email" required autofocus
-                        autocomplete="username" class="input">
-                    <label for="email" class="placeholder" value="{{ __('Email') }}" data-icon="e">Email Address</label>
-                    <div class="input-group">
-                        <input id="password" name="password" required autocomplete="current-password" type="password"
-                            class="input_1">
-                        <label for="password" data-icon="p" value="{{ __('Password') }}"
-                            class="placeholder_1">Password</label>
-                        <button type="submit" name="login" class="submitbtn" value="submit">&nbsp;&nbsp;<i
-                                class="fa-solid fa-right-to-bracket"></i>&nbsp;{{ __('Log in') }}&nbsp;</button>
-                        <div class="keep_login">
-                            <p class="keeplogin">
-                                <input type="checkbox" class="checkbox-input" name="remember_me" id="remember"
-                                    value="loginkeeping" />
-                                <label for="remember_me" class="checkbox-label">{{ __('Remember me') }}</label>
-                            </p>
+                    <input id="name" name="name" type="text" value="{{ old('name') }}" required autofocus autocomplete="name" class="input">
+                    <label for="name" class="placeholder" data-icon="u">Name</label>
+                </div>
 
+                <!-- Email -->
+                <div class="input-group">
+                    <input id="email" name="email" type="email" value="{{ old('email') }}" required autocomplete="email" class="input">
+                    <label for="email" class="placeholder" data-icon="e">Email Address</label>
+                </div>
+
+                <!-- Password -->
+                <div class="input-group">
+                    <input id="password" name="password" type="password" required autocomplete="new-password" class="input_1">
+                    <label for="password" class="placeholder_1" data-icon="p">Password</label>
+                </div>
+
+                <!-- Confirm Password -->
+                <div class="input-group">
+                    <input id="password_confirmation" name="password_confirmation" type="password" required autocomplete="new-password" class="input_1">
+                    <label for="password_confirmation" class="placeholder_1" data-icon="p">Confirm Password</label>
+                </div>
+            </div>
+
+            <!-- Submit Button -->
+            <button type="submit" class="submitbtn">
+                <i class="fa-solid fa-user-plus"></i>&nbsp;{{ __('Register') }}
+            </button>
+
+            <p class="member">Already a Member?</p>
+            <a href="{{ route('login') }}" class="to_login">
+                <button type="button" class="loginbtn">
+                    <i class="fa-solid fa-right-from-bracket"></i>&nbsp;Go to Log in
+                </button>
+            </a>
+
+            <!-- Terms and Privacy -->
+            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <x-label for="terms">
+                        <div class="flex items-center">
+                            <x-checkbox name="terms" id="terms" required />
+                            <div class="ms-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                    'terms_of_service' => '<a target="_blank" href="' . route('terms.show') . '" class="underline text-sm text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Terms of Service') . '</a>',
+                                    'privacy_policy' => '<a target="_blank" href="' . route('policy.show') . '" class="underline text-sm text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' . __('Privacy Policy') . '</a>',
+                                ]) !!}
+                            </div>
                         </div>
-
-                        <div class="rmember">
-                            <p class="member">Not a member yet?</p>
-                        </div>
-
-                        <div class="signbtn">
-                            <a href="{{ route('register') }}" class="to_register">
-                                <button type="button" class="signupbtn">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i
-                                        class="fa-solid fa-user-plus"></i>&nbsp;Signup</button>
-                            </a>
-                        </div>
-                        <div class="flex items-center justify-end mt-4">
-                            @if (Route::has('password.request'))
-                                <a class="forgotpass" href="{{ route('password.request') }}">
-                                    {{ __('Forgot your password?') }}
-                                </a>
-                            @endif
-
-
-
-
-                            <script type="text/javascript">
-                                $(document).ready(function () {
-                                    $('#signin').tooltip('show');
-                                    $('#signin').tooltip('hide');
-                                });
-                            </script>
+                    </x-label>
+                </div>
+            @endif
         </form>
     </div>
-    <script src="/images/script.js"></script>
+
+    <!-- Secure JS -->
+    <script src="{{ secure_asset('images/script.js') }}"></script>
 </body>
 
 </html>
