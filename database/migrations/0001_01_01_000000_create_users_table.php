b406f3f557c3bcb62afe32b6aa1159f2e87aeff9
moses848484 <moses.blake.simataa@gmail.com>
2025-07-22 06:54:14 +0200

    ddede

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -24,7 +23,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable()->after('email');
+            $table->string('profile_photo_path', 2048)->nullable();
+
             $table->timestamps();
         });
 
