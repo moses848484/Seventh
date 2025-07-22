387f8e18ebc1a4276243899e8ea22ce3dcff31ae
moses848484 <moses.blake.simataa@gmail.com>
2025-07-22 15:27:59 +0200

    dfdf

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,3 @@
 import './bootstrap';
+import 'livewire-vite-plugin/resources/js/livewire-hot-reload';
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import livewire from 'livewire-vite-plugin'; // ✅ Add this line
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-               
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
+        livewire(), // ✅ Add this plugin
     ],
 });
