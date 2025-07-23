31b720b540b22aaf6f6c9f824436215c8e62e582
moses848484 <moses.blake.simataa@gmail.com>
2025-07-24 00:57:07 +0200

    sdsds

--- config/livewire.php
+++ config/livewire.php
@@ -63,16 +63,19 @@ return [
     |
     */
 
-'temporary_file_upload' => [
-    'disk' => 'public',
-    'directory' => 'livewire-tmp',
-    'rules' => ['required', 'file', 'max:12288'], // 12MB
-    'middleware' => null,
-    'preview_mimes' => [...], // okay as-is
-    'max_upload_time' => 5,
-    'cleanup' => true,
-],
-
+    'temporary_file_upload' => [
+        'disk' => 'public',        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => ['required', 'file', 'mimes:png,jpg', 'max:2048'], // 2MB limit   // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => 'livewire-tmp',   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => 'throttle:60,1',  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
+        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
+    ],
 
     /*
     |---------------------------------------------------------------------------
