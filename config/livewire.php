cd401f3b97649a6e8fe55d32afadde1a4fe9bf42
moses848484 <moses.blake.simataa@gmail.com>
2025-07-24 00:56:13 +0200

    wdeded

--- config/livewire.php
+++ config/livewire.php
@@ -63,19 +63,16 @@ return [
     |
     */
 
-    'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-        ],
-        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
-        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
-    ],
+'temporary_file_upload' => [
+    'disk' => 'public',
+    'directory' => 'livewire-tmp',
+    'rules' => ['required', 'file', 'max:12288'], // 12MB
+    'middleware' => null,
+    'preview_mimes' => [...], // okay as-is
+    'max_upload_time' => 5,
+    'cleanup' => true,
+],
+
 
     /*
     |---------------------------------------------------------------------------
