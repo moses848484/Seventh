ebb737b7716a2075d184a240c245ad2e83ae1df6
moses848484 <moses.blake.simataa@gmail.com>
2025-07-24 03:22:26 +0200

    ssds

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // ✅ Add this line
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
+    public function boot(): void
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
