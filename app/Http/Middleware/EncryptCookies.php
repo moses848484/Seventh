1cd96e30fa0dcd85e1e186275d09c04576c51139
moses848484 <moses.blake.simataa@gmail.com>
2025-07-24 15:19:44 +0200

    sdsd

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -3,9 +3,16 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+use Illuminate\Support\Facades\Log;
 
 class EncryptCookies extends Middleware
 {
+    public function __construct()
+    {
+        Log::info('EncryptCookies middleware is active.');
+        parent::__construct(app());
+    }
+
     /**
      * The names of the cookies that should not be encrypted.
      *
