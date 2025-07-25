4f17e58ed585c9b827a529c255fb85357c5f9a91
moses848484 <moses.blake.simataa@gmail.com>
2025-07-25 02:44:59 +0200

    wewedf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,11 @@
     @stack('modals')
 
     @livewireScripts
+    <!-- Alpine Plugins -->
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
 
+    <!-- Alpine Core -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </body>
 
 </html>
