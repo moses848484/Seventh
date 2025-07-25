7b0d6111673ae0cef46b17d106bd399eb01a30d9
moses848484 <moses.blake.simataa@gmail.com>
2025-07-26 00:42:25 +0200

    cdcdcdddd

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -25,9 +25,8 @@
                                         <button type="button"
                                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                             {{ Auth::user()->currentTeam->name }}
-                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                stroke="currentColor">
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                             </svg>
@@ -39,8 +38,7 @@
                                         <div class="block px-4 py-2 text-xs text-gray-400">
                                             {{ __('Manage Team') }}
                                         </div>
-                                        <x-dropdown-link
-                                            href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                        <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
                                             {{ __('Team Settings') }}
                                         </x-dropdown-link>
                                         @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
@@ -63,25 +61,23 @@
                         </div>
                     @endif
 
-                    <!-- Settings Dropdown -->
-                       <div class="ms-3 relative d-block d-sm-none" style="right: -60px; top: -95px;">
+                    <!-- Settings Dropdown (Desktop only) -->
+                    <div class="ms-3 relative d-none d-lg-block" style="right: 20px; top: -95px;">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                     <button
                                         class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                         <img class="h-8 w-8 rounded-full object-cover"
-                                            src="{{ Auth::user()->profile_photo_url }}"
-                                            alt="{{ Auth::user()->name }}" />
+                                            src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
                                 @else
                                     <span class="inline-flex rounded-md">
                                         <button type="button"
                                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                             {{ Auth::user()->name }}
-                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                stroke="currentColor">
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                             </svg>
@@ -111,6 +107,7 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -148,11 +145,13 @@
                     class="absolute right-0 z-10 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
                     <div class="bg-white p-3 border-t border-gray-200">
                         <div class="mt-3 space-y-1">
-                            <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                            <x-responsive-nav-link href="{{ route('profile.show') }}"
+                                :active="request()->routeIs('profile.show')">
                                 {{ __('Profile') }}
                             </x-responsive-nav-link>
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
+                                <x-responsive-nav-link href="{{ route('api-tokens.index') }}"
+                                    :active="request()->routeIs('api-tokens.index')">
                                     {{ __('API Tokens') }}
                                 </x-responsive-nav-link>
                             @endif
@@ -354,7 +353,8 @@
                             {{ __('Team Settings') }}
                         </x-responsive-nav-link>
                         @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                            <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
+                            <x-responsive-nav-link href="{{ route('teams.create') }}"
+                                :active="request()->routeIs('teams.create')">
                                 {{ __('Create New Team') }}
                             </x-responsive-nav-link>
                         @endcan
