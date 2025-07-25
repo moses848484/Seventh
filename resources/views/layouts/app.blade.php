08b1cc92f52087c635ad13bef4f46717ba60cacc
moses848484 <moses.blake.simataa@gmail.com>
2025-07-25 02:58:30 +0200

    defef

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,7 @@
     @livewireScripts
     <!-- Alpine Plugins -->
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
+    @livewire('profile.update-profile-information-form')
 
     <!-- Alpine Core -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
