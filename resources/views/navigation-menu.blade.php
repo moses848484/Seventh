2f95da0dbafd509fcc775305a6c78b329781823e
moses848484 <moses.blake.simataa@gmail.com>
2025-07-26 02:14:09 +0200

    wdwdwdw

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -63,7 +63,7 @@
 
                     <!-- Settings Dropdown (Desktop only) - Fixed positioning -->
                        <!-- Settings Dropdown -->
-                    <div class="ms-3 relative" style=" right: 20px; ">
+                    <div class="ms-3 relative" style=" right: 20px; top: -95px;">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
