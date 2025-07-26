8845a26942a29bb374a05c6dbc5f7e59ba247219
moses848484 <moses.blake.simataa@gmail.com>
2025-07-26 19:17:55 +0200

    sdsd

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -122,7 +122,7 @@
         <div class="offcanvas-header border-bottom">
             <h5 id="offcanvasMenuLabel" class="fw-bold">Menu</h5>
             <!-- Custom close button with Font Awesome icon -->
-            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-dismiss="offcanvas"
+            <button type="button" class="btn btn-sm btn-outline-light" data-bs-dismiss="offcanvas"
                 aria-label="Close">
                 <i class="fa-solid fa-xmark"></i>
             </button>
