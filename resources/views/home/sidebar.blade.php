ad84ed19caafb8834d18757daebf17892e20f64c
moses848484 <moses.blake.simataa@gmail.com>
2025-07-27 04:44:47 +0200

    ssdsd

--- resources/views/home/sidebar.blade.php
+++ resources/views/home/sidebar.blade.php
@@ -1,14 +1,16 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-    <a class="sidebar-brand brand-logo" href="{{ url('/redirect') }}"><img src="admin/assets/images/faces/sda3.png" alt="logo" /></a>
-    <a class="sidebar-brand brand-logo-mini" href="{{ url('/redirect') }}"><img src="admin/assets/images/faces/sda4.png" alt="logo" /></a>
+    <a class="sidebar-brand brand-logo" href="{{ url('/redirect') }}"><img src="admin/assets/images/faces/sda3.png"
+        alt="logo" /></a>
+    <a class="sidebar-brand brand-logo-mini" href="{{ url('/redirect') }}"><img src="admin/assets/images/faces/sda4.png"
+        alt="logo" /></a>
   </div>
   <ul class="nav">
 
     <li class="nav-item nav-category">
       <span class="nav-link">Navigation</span>
     </li>
-    
+
     <li class="nav-item menu-items {{ request()->is('/redirected') ? 'active' : '' }}">
       <a class="nav-link" href="{{ url('/redirected') }}">
         <span class="menu-icon">
@@ -16,6 +18,35 @@
         </span>
         <span class="menu-title">Dashboard</span>
       </a>
+    </li>
+
+    <li
+      class="nav-item menu-items {{ request()->is('view_members') || request()->is('see_members') || request()->is('update_member/*') ? 'active' : '' }}">
+      <a class="nav-link" data-toggle="collapse" href="#ui-basic"
+        aria-expanded="{{ request()->is('view_members') || request()->is('see_members') ? 'true' : 'false' }}"
+        aria-controls="ui-basic">
+        <span class="menu-icon">
+          <i class="fa-solid fa-users fa-3x"></i>
+        </span>
+        <span class="menu-title">Manage Members</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ request()->is('view_members') || request()->is('see_members') ? 'show' : '' }}"
+        id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ request()->is('view_members') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('view_members') }}">
+              <i class="fa-solid fa-user"></i>&nbsp;Register Members
+            </a>
+          </li>
+
+          <li class="nav-item {{ request()->is('see_members') || request()->is('update_member/*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('see_members') }}">
+              <i class="fa-solid fa-eye"></i>&nbsp;View Members
+            </a>
+          </li>
+        </ul>
+      </div>
     </li>
 
     <li class="nav-item menu-items {{ request()->is('member_givings') ? 'active' : '' }}">
@@ -40,8 +71,8 @@
 </nav>
 
 <script>
-      function previewImage(event) {
-        const image = document.getElementById('profileImage');
-        image.src = URL.createObjectURL(event.target.files[0]);
-      }
-    </script>
+  function previewImage(event) {
+    const image = document.getElementById('profileImage');
+    image.src = URL.createObjectURL(event.target.files[0]);
+  }
+</script>
