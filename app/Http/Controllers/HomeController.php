0f6d99d4c0dcc598a822f0f0afc626a2c6065304
moses848484 <moses.blake.simataa@gmail.com>
2025-07-27 18:17:10 +0200

    rfffr

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Members;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
-
 use App\Models\User;
-
 use App\Models\Givings;
 
 class HomeController extends Controller
@@ -18,27 +15,34 @@ class HomeController extends Controller
     {
         return view('home.userpage');
     }
+
     public function redirect()
     {
         // Check if the user is authenticated
         if (!Auth::check()) {
-            return redirect()->route('login'); // Redirect to login if not authenticated
+            return redirect()->route('login');
         }
 
         $usertype = Auth::user()->usertype;
 
         if ($usertype == '1') {
             $users = User::all();
-            $userCount = User::count(); // Count the number of users
+            $userCount = User::count();
             $data = members::all();
-            $memberCount = members::count(); // Count the number of members
-            $membersCount = members::where('registeras', 'member')->count(); // Count the number of members
-            $visitorCount = members::where('registeras', 'visitor')->count(); // Count the number of visitors
-            $singlesCount = members::where('marital', 'single')->count(); // Count the number of single people
-            $marriedCount = members::where('marital', 'married')->count(); // Count the number of married people
-            $divorcedCount = members::where('marital', 'divorced')->count(); // Count the number of divorced people
-            // Pass the data and member count to the admin.home view
-            $user = Auth::user();
+            $memberCount = members::count();
+            $membersCount = members::where('registeras', 'member')->count();
+            $visitorCount = members::where('registeras', 'visitor')->count();
+            $singlesCount = members::where('marital', 'single')->count();
+            $marriedCount = members::where('marital', 'married')->count();
+            $divorcedCount = members::where('marital', 'divorced')->count();
+            
+            // Show welcome notification only once per session
+            $welcomeKey = 'welcomed_user_' . Auth::id();
+            if (!session()->has($welcomeKey)) {
+                $user = Auth::user();
+                notify()->success('Welcome, ' . $user->name . '!');
+                session([$welcomeKey => true]);
+            }
           
             return view('admin.home', [
                 'data' => $data,
@@ -52,9 +56,14 @@ class HomeController extends Controller
                 'divorcedCount' => $divorcedCount
             ]);
         } else {
-            // Redirect to home.userpage view for regular users
-            $user = Auth::user();
-       
+            // Show welcome notification only once per session
+            $welcomeKey = 'welcomed_user_' . Auth::id();
+            if (!session()->has($welcomeKey)) {
+                $user = Auth::user();
+                notify()->success('Welcome, ' . $user->name . '!');
+                session([$welcomeKey => true]);
+            }
+            
             return view('home.memberhome');
         }
     }
@@ -63,22 +72,21 @@ class HomeController extends Controller
     {
         // Check if the user is authenticated
         if (!Auth::check()) {
-            return redirect()->route('login'); // Redirect to login if not authenticated
+            return redirect()->route('login');
         }
 
         $usertype = Auth::user()->usertype;
 
         if ($usertype == '1') {
             $users = User::all();
-            $userCount = User::count(); // Count the number of users
+            $userCount = User::count();
             $data = members::all();
-            $memberCount = members::count(); // Count the number of members
-            $membersCount = members::where('registeras', 'member')->count(); // Count the number of members
-            $visitorCount = members::where('registeras', 'visitor')->count(); // Count the number of visitors             
-            $singlesCount = members::where('marital', 'single')->count(); // Count the number of single people
-            $marriedCount = members::where('marital', 'married')->count(); // Count the number of married people
-            $divorcedCount = members::where('marital', 'divorced')->count(); // Count the number of divorced people
-            // Pass the data and member count to the admin.home view
+            $memberCount = members::count();
+            $membersCount = members::where('registeras', 'member')->count();
+            $visitorCount = members::where('registeras', 'visitor')->count();             
+            $singlesCount = members::where('marital', 'single')->count();
+            $marriedCount = members::where('marital', 'married')->count();
+            $divorcedCount = members::where('marital', 'divorced')->count();
 
             return view('admin.home', [
                 'data' => $data,
@@ -92,7 +100,6 @@ class HomeController extends Controller
                 'divorcedCount' => $divorcedCount
             ]);
         } else {
-            // Redirect to home.memberhome view for regular users
             return view('home.memberhome');
         }
     }
@@ -104,15 +111,22 @@ class HomeController extends Controller
 
     public function update_account()
     {
-        $data = Members::all(); // Fetch all members
-        return view('home.update', compact('data')); // Pass data to the view
+        $data = Members::all();
+        return view('home.update', compact('data'));
     }
-
-
 
     public function member_givings()
     {
         return view('home.givings');
     }
 
+    /**
+     * Clear welcome session when user logs out
+     * Add this method and call it from your logout process
+     */
+    public function clearWelcomeSession()
+    {
+        $welcomeKey = 'welcomed_user_' . Auth::id();
+        session()->forget($welcomeKey);
+    }
 }
