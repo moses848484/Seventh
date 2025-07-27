f293a5d9bba04f4d7855486fe78031e50a03dfd7
moses848484 <moses.blake.simataa@gmail.com>
2025-07-27 05:13:26 +0200

    sasasa

--- resources/views/admin/css.blade.php
+++ resources/views/admin/css.blade.php
@@ -1,49 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Material Design Icons -->
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/vendors/mdi/css/materialdesignicons.min.css" />
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <title>SDA Admin</title>
+<!-- FontAwesome (choose one version only) -->
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/css/fontawesome-free-6.6.0-web/css/all.min.css" />
 
-  <!-- Material Design Icons -->
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/vendors/mdi/css/materialdesignicons.min.css" />
+<!-- Plugin CSS -->
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/vendors/jvectormap/jquery-jvectormap.css" />
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/vendors/flag-icon-css/css/flag-icon.min.css" />
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.carousel.min.css" />
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css" />
 
-  <!-- FontAwesome (choose one version only) -->
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/css/fontawesome-free-6.6.0-web/css/all.min.css" />
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+  href="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/css/bootstrap.min.css" />
 
-  <!-- Plugin CSS -->
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/vendors/jvectormap/jquery-jvectormap.css" />
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/vendors/flag-icon-css/css/flag-icon.min.css" />
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.carousel.min.css" />
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css" />
+<!-- Your custom styles -->
+<link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style.css" />
+<link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style1.css" />
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet"
-    href="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+<!-- Favicon -->
+<link rel="shortcut icon" href="https://seventh-production.up.railway.app/admin/assets/images/sda3.png" />
 
-  <!-- Your custom styles -->
-  <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style.css" />
-  <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style1.css" />
-
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="https://seventh-production.up.railway.app/admin/assets/images/sda3.png" />
-
-  <!-- Your custom script -->
-  <script src="https://seventh-production.up.railway.app/images/script.js" defer></script>
-</head>
-
-<body>
-  <!-- Your page content goes here -->
-
-  <!-- Bootstrap JS Bundle (includes Popper) -->
-  <script src="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
-</body>
-
-</html>
+<!-- Your custom script -->
+<script src="https://seventh-production.up.railway.app/images/script.js" defer></script>
+<script src="/images/script.js"></script>
+<!-- Bootstrap JS Bundle (includes Popper) -->
+<script src="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+  defer></script>
