3a1b2dd68b3dea4f9e524156d35145a34fa39ce3
moses848484 <moses.blake.simataa@gmail.com>
2025-07-27 05:54:22 +0200

    sdsds

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 
-
+    // routes/web.php
+    Route::put('/user/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('user-profile-information.update');
     Route::delete('/user/profile-photo', [App\Http\Controllers\ProfilePhotoController::class, 'destroy'])->name('profile-photo.remove')->middleware(['auth']);
 
 
