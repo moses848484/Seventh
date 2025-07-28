b22089ff254daec5348e7e24f9e7310654217338
moses848484 <moses.blake.simataa@gmail.com>
2025-07-28 20:27:18 +0200

    fefef

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -232,7 +232,7 @@
                     </div>
                 </li>
                 <!-- User Profile/Authentication Dropdown -->
-                       <li class="nav-item dropdown ms-5">
+                       <li class="nav-item dropdown">
                     <x-app-layout class="bg-white">
                     </x-app-layout>
                 </li>
