4eb8fc0fda43b5c17a3dc2b861042817a37292b9
moses848484 <moses.blake.simataa@gmail.com>
2025-07-29 07:23:48 +0200

    vfvfv

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,3 @@
-<!-- Bootstrap 5.3 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-..." crossorigin="anonymous">
 <nav x-data="{ open: false }" class="">
     <!-- Primary Navigation Menu -->
     <div class="">
