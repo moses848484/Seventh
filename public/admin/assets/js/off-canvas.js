cf4a47c3b1f76db9846fb81a75d26b7e72882d25
moses848484 <moses.blake.simataa@gmail.com>
2025-08-02 12:38:33 +0200

    deded

--- public/admin/assets/js/off-canvas.js
+++ public/admin/assets/js/off-canvas.js
@@ -1,17 +1,13 @@
-(function($) {
-  'use strict';
-  $(function() {
-    // Toggle sidebar
-    $('[data-bs-toggle="offcanvas"]').on("click", function() {
-      $('.sidebar-offcanvas').toggleClass('active');
-    });
+(function ($) {
+    "use strict";
+    $(function () {
+        $('[data-bs-toggle="offcanvas"]').on("click", function () {
+            $(".sidebar-offcanvas").toggleClass("active");
 
-    // Prevent dropdown toggles when sidebar is collapsed
-    $('.sidebar-offcanvas .nav-link[data-bs-toggle="collapse"]').on('click', function(e) {
-      if (!$('.sidebar-offcanvas').hasClass('active')) {
-        e.preventDefault(); // block dropdown from opening
-        e.stopPropagation();
-      }
+            // If sidebar just got closed, hide all open dropdowns
+            if (!$(".sidebar-offcanvas").hasClass("active")) {
+                $(".sidebar-offcanvas .collapse.show").removeClass("show");
+            }
+        });
     });
-  });
 })(jQuery);
