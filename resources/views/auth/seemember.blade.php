1ab6c840f8e239a79d686a00e9c5ab3635210f0f
moses848484 <moses.blake.simataa@gmail.com>
2025-08-05 23:53:46 +0200

    sdsd

--- resources/views/auth/seemember.blade.php
+++ resources/views/auth/seemember.blade.php
@@ -6,213 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Church Manager" />
     <meta name="keywords" content="Church, Manager, Member registration, Donation, Tithe Manager" />
-    <link rel="stylesheet" href="<?php echo asset('css/addmember.css'); ?>" type="text/css">
-    <link rel="stylesheet" href="<?php echo asset('css/fontawesome-free-6.5.2-web/css/all.min.css'); ?>" type="text/css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>View Church Members</title>        
+    <link rel="stylesheet" href="<?php echo asset('css/viewmember.css') ?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/fontawesome-free-6.5.2-web/css/all.min.css') ?>" type="text/css">
+    <title>View Equipment</title>
     <style>
-        /* Custom Autofill input styling */
+        /* Autofill input styling */
         input:-webkit-autofill,
         input:-webkit-autofill:hover,
         input:-webkit-autofill:focus,
         input:-webkit-autofill:active {
             -webkit-box-shadow: 0 0 0 30px white inset !important;
+            /* Changes background color */
             box-shadow: 0 0 0 30px white inset !important;
             -webkit-text-fill-color: #04AA6D !important;
-        }
-
-        /* Floating Label Styles */
-        .form-floating {
-            position: relative;
-        }
-
-        .form-floating input {
-            padding: 1rem 0.75rem;
-            padding-top: 1.5rem;
-        }
-
-        .form-floating label {
-            position: absolute;
-            top: 45%;
-            left: 3%;
-            transform: translateY(-50%);
-            transition: all 0.2s ease-in-out;
-            color: #04AA6D;
-            pointer-events: none;
-        }
-
-        .form-floating input:focus+label,
-        .form-floating input:not(:placeholder-shown)+label {
-            top: 25%;
-            transform: translateY(-100%);
-            font-size: 0.80rem;
-            color: #04AA6D;
-            background: white;
-        }
-
-        /* Adjustments for input border */
-        .form-control {
-            border: 1px solid #ced4da;
-            border-radius: 0.25rem;
-            color: #04AA6D;
-        }
-
-        .form-control:focus {
-            border: 1px solid #ced4da;
-            border-radius: 0.25rem;
-            color: #04AA6D;
-        }
-
-        .text-primary {
-            color: #e4af00 !important;
-        }
-
-        /* Responsive adjustments */
-        @media (max-width: 1200px) {
-            .form-floating input {
-                padding: 0.7rem 0.25rem;
-                padding-top: 1rem;
-                margin-bottom: 18px;
-            }
-
-            .form-floating label {
-                font-size: 0.75rem;
-                margin-top: -5px;
-                margin-left: 7px;
-            }
-
-            .form-select {
-                color: #04AA6D;
-                border-color: #04AA6D;
-            }
-
-            .form-floating label:focus {
-                font-size: 0.9rem;
-                padding: 0.75rem 1rem;
-            }
-
-            .btn {
-                font-size: 1rem;
-            }
-
-            .container {
-                padding: 2rem;
-            }
-
-            .footer span {
-                font-size: 0.85rem;
-            }
-        }
-
-        /* Responsive adjustments */
-        @media (max-width: 1200px) {
-            .form-floating input {
-                padding: 0.7rem 0.25rem;
-                padding-top: 1rem;
-                margin-bottom: 18px;
-            }
-
-            .form-floating label {
-                font-size: 0.75rem;
-                margin-top: -5px;
-                margin-left: 7px;
-            }
-
-            .form-floating label:focus {
-                font-size: 0.9rem;
-                padding: 0.75rem 1rem;
-            }
-
-            .btn {
-                font-size: 1rem;
-            }
-
-            .container {
-                padding: 2rem;
-            }
-
-            .footer span {
-                font-size: 0.85rem;
-            }
-        }
-
-        /* Responsive adjustments */
-        @media (max-width: 768px) {
-            .form-floating input {
-                padding: 0.7rem 0.25rem;
-                padding-top: 1rem;
-                margin-bottom: 18px;
-            }
-
-            .form-floating label {
-                font-size: 0.75rem;
-                margin-top: -5px;
-                margin-left: 7px;
-            }
-
-            .form-floating label:focus {
-                font-size: 0.9rem;
-                padding: 0.75rem 1rem;
-            }
-
-            .btn {
-                font-size: 1rem;
-            }
-
-            .container {
-                padding: 2rem;
-            }
-
-            .footer span {
-                font-size: 0.85rem;
-            }
-        }
-
-        @media (max-width: 576px) {
-            .form-floating input {
-                padding: 0.7rem 0.25rem;
-                padding-top: 2rem;
-                margin-bottom: 18px;
-            }
-
-            .form-floating label {
-                font-size: 1rem;
-                margin-top: -5px;
-                margin-left: 7px;
-            }
-
-            .form-floating label:focus {
-                font-size: 0.9rem;
-                padding: 0.75rem 1rem;
-            }
-
-            .container {
-                padding: 1rem;
-            }
-
-            .textcolor {
-                color: #04AA6D;
-            }
-
-            .footer span {
-                font-size: 0.75rem;
-            }
-        }
-
-        /* Map styles */
-        #map {
-            height: 400px;
-            /* Set the desired height */
-            width: 100%;
-            /* Full width */
-            margin: 20px 0;
-            /* Margin for spacing */
-        }
-
-        .content-wrapper {
-            margin: 5px;
-            margin-top: 2px;
-            width: 101%;
+            /* Changes text color */
         }
     </style>
 </head>
@@ -220,22 +27,21 @@
 <body>
     <footer class="footer2">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted4 d-block text-center text-sm-left d-sm-inline-block">
-                <i class="fa-solid fa-user"></i>&nbsp;Church Members
-            </span>
+            <span class="text-muted4 d-block text-center text-sm-left d-sm-inline-block"><i
+                    class="fa-solid fa-user"></i>&nbsp;Church Members</span>
         </div>
 
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted3 d-block text-center text-sm-left d-sm-inline-block">
-                <i class="fa-solid fa-circle-info"></i>&nbsp;Number Of Members:<span class="badge badge-info">
-                    {{ $memberCount }}</span>
-            </span>
+            <span class="text-muted3 d-block text-center text-sm-left d-sm-inline-block"><i
+                    class="fa-solid fa-circle-info"></i>&nbsp;Number Of Members:<span class="badge badge-info">
+                    {{ $memberCount }}</span></span>
         </div>
 
         <div class="search-container">
-            <input type="text" id="search-input" class="form-control" placeholder="Search Members"
+            <input type="text" id="search-input" class="form-control" placeholder="Search Equipment"
                 onkeyup="searchTable()">
         </div>
+
     </footer>
 
     <div id="block_bg" class="block">
@@ -316,7 +122,8 @@
             </span>
         </div>
     </footer>
-
+    </div>
+    <script src="/images/script.js"></script>
     <script>
         function searchTable() {
             // Get the search input value
