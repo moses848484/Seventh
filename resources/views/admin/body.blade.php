d5ce249d8a5d5630694bfeb2749e9d254b056199
moses848484 <moses.blake.simataa@gmail.com>
2025-08-06 01:01:57 +0200

    scsc

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -213,13 +213,13 @@
                         <div class="card-body py-0 px-0 px-sm-3">
                             <div class="row align-items-center g-0">
                                 <!-- Image Column - Properly positioned to left -->
-                                <div class="col-4 col-sm-3 col-xl-2">
+                                <div class="col-3 col-sm-3 col-xl-2">
                                     <img src="admin/assets/images/bible4.jpg" class="gradient-corona-img img-fluid"
                                         alt="Bible Image" loading="lazy">
                                 </div>
 
                                 <!-- Verse Content Column -->
-                                <div class="col-5 col-sm-7 col-xl-8 p-0">
+                                <div class="col-6 col-sm-7 col-xl-8 p-0">
                                     <div id="verse-of-the-day" style="color: white;"></div>
                                 </div>
 
@@ -237,162 +237,163 @@
                 </div>
             </div>
 
-    <div class="card1">
-        <div class="row">
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <div class="span2"><br />
-                                    <i class="fa-solid fa-users fa-3x"></i><br />
-                                </div>
-                                <div class="d-flex align-items-center align-self-start">
+            <div class="card1">
+                <div class="row">
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="span2"><br />
+                                            <i class="fa-solid fa-users fa-3x"></i><br />
+                                        </div>
+                                        <div class="d-flex align-items-center align-self-start">
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="icon icon-box-success">
+                                            <a class="nav-link" href="{{ url('see_members') }}">
+                                                <span class="mdi mdi-arrow-right icon-item"></span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success">
-                                    <a class="nav-link" href="{{ url('see_members') }}">
-                                        <span class="mdi mdi-arrow-right icon-item"></span>
-                                    </a>
-                                </div>
+                            <div class="footer1">
+                                <h6 class="text-muted2 font-weight-normal">Member Details</h6>
                             </div>
                         </div>
                     </div>
-                    <div class="footer1">
-                        <h6 class="text-muted2 font-weight-normal">Member Details</h6>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <div class="span2"><br />
-                                    <i class="fa-solid fa-user-plus fa-3x"></i><br />
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="span2"><br />
+                                            <i class="fa-solid fa-user-plus fa-3x"></i><br />
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="icon icon-box-success">
+                                            <a class="nav-link" href="{{ url('view_members') }}">
+                                                <span class="mdi mdi-arrow-right icon-item"></span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success">
-                                    <a class="nav-link" href="{{ url('view_members') }}">
-                                        <span class="mdi mdi-arrow-right icon-item"></span>
-                                    </a>
-                                </div>
+                            <div class="footer1">
+                                <h6 class="text-muted2 font-weight-normal">Add Members</h6>
                             </div>
                         </div>
                     </div>
-                    <div class="footer1">
-                        <h6 class="text-muted2 font-weight-normal">Add Members</h6>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <div class="span2"><br />
-                                    <i class="fa fa-calendar fa-3x"></i><br />
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="span2"><br />
+                                            <i class="fa fa-calendar fa-3x"></i><br />
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="icon icon-box-success">
+                                            <span class="mdi mdi-arrow-right icon-item"></span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success">
-                                    <span class="mdi mdi-arrow-right icon-item"></span>
-                                </div>
+                            <div class="footer1">
+                                <h6 class="text-muted2 font-weight-normal">Upcoming Birthdays</h6>
                             </div>
                         </div>
                     </div>
-                    <div class="footer1">
-                        <h6 class="text-muted2 font-weight-normal">Upcoming Birthdays</h6>
-                    </div>
-                </div>
-            </div>
 
-            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <div class="span2"><br />
-                                    <i class="fa fa-money-check-alt fa-3x"></i><br />
+                    <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="span2"><br />
+                                            <i class="fa fa-money-check-alt fa-3x"></i><br />
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div class="icon icon-box-success">
+                                            <a class="nav-link" href="{{ url('see_givings') }}">
+                                                <span class="mdi mdi-arrow-right icon-item"></span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
-                                <div class="icon icon-box-success">
-                                    <a class="nav-link" href="{{ url('see_givings') }}">
-                                        <span class="mdi mdi-arrow-right icon-item"></span>
-                                    </a>
-                                </div>
+                            <div class="footer1">
+                                <h6 class="text-muted2 font-weight-normal">Givings</h6>
                             </div>
                         </div>
                     </div>
-                    <div class="footer1">
-                        <h6 class="text-muted2 font-weight-normal">Givings</h6>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4 grid-margin stretch-card">
+                        <div class="card h-100 d-flex flex-column">
+                            <div class="card-body">
+                                <canvas id="myChart" class="member-chart"></canvas>
+                            </div>
+                            <div class="footer1 mt-auto">
+                                <h6 class="text-muted2 font-weight-normal">Member Chart</h6>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 grid-margin stretch-card">
+                        <div class="card h-100 d-flex flex-column">
+                            <div class="card-body">
+                                <canvas id="myUsers" class="member-chart1"></canvas>
+                            </div>
+                            <div class="footer1 mt-auto">
+                                <h6 class="text-muted2 font-weight-normal">User Chart</h6>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 grid-margin stretch-card">
+                        <div class="card h-100 d-flex flex-column" style="height: 530px;">
+                            <div class="card-body">
+                                <!-- Embedded Bible -->
+                                <biblia:bible class="biblia-bible" layout="normal" resource="leb" width="100%"
+                                    height="100%" startingReference="Ge1.1"></biblia:bible>
+                            </div>
+                            <!-- Footer -->
+                            <div class="footer1 mt-auto">
+                                <h6 class="text-muted2 font-weight-normal">Holy Bible</h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- content-wrapper ends -->
+
+            <!-- partial:partials/_footer.html -->
+            <footer class="footer">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted1 d-block text-center text-sm-left d-sm-inline-block">Copyright ©
+                        University
+                        SDA Church 2024</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-white">
+                        Computer Science Dept
+                        <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank"
+                            class="text-white">
+                            Computer Systems Engineering
+                        </a> from University Of Zambia
+                    </span>
+                </div>
+            </footer>
+            <!-- partial -->
         </div>
-
-        <div class="row">
-            <div class="col-md-4 grid-margin stretch-card">
-                <div class="card h-100 d-flex flex-column">
-                    <div class="card-body">
-                        <canvas id="myChart" class="member-chart"></canvas>
-                    </div>
-                    <div class="footer1 mt-auto">
-                        <h6 class="text-muted2 font-weight-normal">Member Chart</h6>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 grid-margin stretch-card">
-                <div class="card h-100 d-flex flex-column">
-                    <div class="card-body">
-                        <canvas id="myUsers" class="member-chart1"></canvas>
-                    </div>
-                    <div class="footer1 mt-auto">
-                        <h6 class="text-muted2 font-weight-normal">User Chart</h6>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 grid-margin stretch-card">
-                <div class="card h-100 d-flex flex-column" style="height: 530px;">
-                    <div class="card-body">
-                        <!-- Embedded Bible -->
-                        <biblia:bible class="biblia-bible" layout="normal" resource="leb" width="100%" height="100%"
-                            startingReference="Ge1.1"></biblia:bible>
-                    </div>
-                    <!-- Footer -->
-                    <div class="footer1 mt-auto">
-                        <h6 class="text-muted2 font-weight-normal">Holy Bible</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- content-wrapper ends -->
-
-    <!-- partial:partials/_footer.html -->
-    <footer class="footer">
-        <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted1 d-block text-center text-sm-left d-sm-inline-block">Copyright ©
-                University
-                SDA Church 2024</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-white">
-                Computer Science Dept
-                <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank" class="text-white">
-                    Computer Systems Engineering
-                </a> from University Of Zambia
-            </span>
-        </div>
-    </footer>
-    <!-- partial -->
-    </div>
-    <!-- main-panel ends -->
+        <!-- main-panel ends -->
     </div>
     <!-- page-body-wrapper ends -->
     </div>
@@ -514,36 +515,47 @@
             }
         });
     </script>
-<!-- Verse of the Day Script -->
-<script>
-    function myVotdCallback(data) {
-        var votdContainer = document.getElementById('verse-of-the-day');
-        if (votdContainer && data && data.votd) {
-            // Create a more formatted display
-            var verseHTML = `
+    <!-- Verse of the Day Script -->
+    <script>
+        function myVotdCallback(data) {
+            var votdContainer = document.getElementById('verse-of-the-day');
+            if (votdContainer && data && data.votd) {
+                // Clean the verse text by removing extra quotes
+                var cleanText = data.votd.text;
+
+                // Remove surrounding quotes if they exist
+                if (cleanText.startsWith('"') && cleanText.endsWith('"')) {
+                    cleanText = cleanText.slice(1, -1);
+                }
+
+                // Remove any double quotes at the beginning or end
+                cleanText = cleanText.replace(/^["']+|["']+$/g, '');
+
+                // Create a more formatted display without adding extra quotes
+                var verseHTML = `
                 <div class="verse-content">
-                    <p class="mb-1">"${data.votd.text}"</p>
+                    <p class="mb-1">"${cleanText}"</p>
                     <small class="verse-reference">- ${data.votd.reference}</small>
                 </div>
             `;
-            votdContainer.innerHTML = verseHTML;
-        } else if (votdContainer) {
-            votdContainer.innerHTML = '<p class="mb-0"><em>Loading daily verse...</em></p>';
-        }
-    }
-    
-    // Error handling for failed script loading
-    window.addEventListener('load', function() {
-        setTimeout(function() {
-            var votdContainer = document.getElementById('verse-of-the-day');
-            if (votdContainer && votdContainer.innerHTML.trim() === '') {
-                votdContainer.innerHTML = '<p class="mb-0"><em>Daily verse temporarily unavailable</em></p>';
+                votdContainer.innerHTML = verseHTML;
+            } else if (votdContainer) {
+                votdContainer.innerHTML = '<p class="mb-0"><em>Loading daily verse...</em></p>';
             }
-        }, 5000); // Wait 5 seconds for the script to load
-    });
-</script>
+        }
 
-<script src="https://www.biblegateway.com/votd/get/?format=json&version=NIV&callback=myVotdCallback"></script>
+        // Error handling for failed script loading
+        window.addEventListener('load', function () {
+            setTimeout(function () {
+                var votdContainer = document.getElementById('verse-of-the-day');
+                if (votdContainer && votdContainer.innerHTML.trim() === '') {
+                    votdContainer.innerHTML = '<p class="mb-0"><em>Daily verse temporarily unavailable</em></p>';
+                }
+            }, 5000); // Wait 5 seconds for the script to load
+        });
+    </script>
+
+    <script src="https://www.biblegateway.com/votd/get/?format=json&version=NIV&callback=myVotdCallback"></script>
 </body>
 
 </html>
