47844ff573fc502fa26f9dbd729455d26368b546
moses848484 <moses.blake.simataa@gmail.com>
2025-08-06 03:34:19 +0200

    scsc

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -363,8 +363,8 @@
                     <div class="col-md-4 col-sm-12 grid-margin stretch-card">
                         <div class="card d-flex flex-column" style="height: 530px;">
                             <!-- Embedded Bible -->
-                            <iframe src="https://www.bible.com/bible/97/GEN.1.NLT" width="100%" height="100%"
-                                allowfullscreen loading="lazy">
+                      <iframe src="https://www.bible.com/bible/97/GEN.1.NLT" width="100%" height="100%"
+                                style="border: none; min-height: 100%;" allowfullscreen loading="lazy">
                             </iframe>
                             <!-- Spacer to push the footer down -->
                             <div class="flex-grow-1"></div>
