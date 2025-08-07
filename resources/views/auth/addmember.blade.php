22f45bb2ac28c0bca4518e83cd115db7ba7b4967
moses848484 <moses.blake.simataa@gmail.com>
2025-08-07 15:36:04 +0200

    sdsd

--- resources/views/auth/addmember.blade.php
+++ resources/views/auth/addmember.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Church Manager" />
     <meta name="keywords" content="Church, Manager, Member registration, Donation, Tithe Manager" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}"> 
     <link rel="stylesheet" href="{{ asset('css/fontawesome-free-6.5.2-web/css/all.min.css') }}" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Registration</title>
@@ -44,7 +43,7 @@
 
         .form-floating input:focus+label,
         .form-floating input:not(:placeholder-shown)+label {
-            top: -30%;
+            top: -25%;
             transform: translateY(-100%);
             font-size: 15px !important;;
             color: #04AA6D !important;;
