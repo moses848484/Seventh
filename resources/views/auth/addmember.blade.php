135f61bf6eb7a34af74d17fe5bb8c6f5d2252b65
moses848484 <moses.blake.simataa@gmail.com>
2025-08-07 06:22:49 +0200

    rerer

--- resources/views/auth/addmember.blade.php
+++ resources/views/auth/addmember.blade.php
@@ -12,12 +12,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Registration</title>
     <style>
-        /* Custom Autofill input styling */
-        input:-webkit-autofill,
+         /* Custom Autofill input styling */
+         input:-webkit-autofill,
         input:-webkit-autofill:hover,
         input:-webkit-autofill:focus,
         input:-webkit-autofill:active {
-       
+            -webkit-box-shadow: 0 0 0 30px white inset !important;
+            box-shadow: 0 0 0 30px white inset !important;
             -webkit-text-fill-color: #04AA6D !important;
         }
 
