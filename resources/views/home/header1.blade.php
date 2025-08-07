d945522622c58cde4605d9991fb72df0a84ca44f
moses848484 <moses.blake.simataa@gmail.com>
2025-08-08 00:37:32 +0200

    ssd

--- resources/views/home/header1.blade.php
+++ resources/views/home/header1.blade.php
@@ -80,10 +80,7 @@
                     style="max-height: 35px; width: auto; display: block;" alt="logo" />
             </a>
 
-            <!-- Toggler button -->
-            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-                <span class="mdi mdi-menu"></span>
-            </button>
+      
 
 
             <ul class="navbar-nav navbar-nav-right">
@@ -232,8 +229,9 @@
                     </div>
                 </li>
                 <!-- User Profile/Authentication Dropdown -->
-                <li class="nav-item dropdown ms-5">
-                    @livewire('navigation-menu')
+                       <li class="nav-item dropdown">
+                    <x-app-layout class="bg-white">
+                    </x-app-layout>
                 </li>
             </ul>
         </div>
