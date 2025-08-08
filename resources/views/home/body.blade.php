b2bb294d55aac49a014aa2ccc50f079d2d556cd1
moses848484 <moses.blake.simataa@gmail.com>
2025-08-08 03:42:10 +0200

    fef

--- resources/views/home/body.blade.php
+++ resources/views/home/body.blade.php
@@ -291,7 +291,7 @@
 </head>
 
 <body>
-    <div class="main-panel">
+<div class="main-panel">
         <div class="content-wrapper">
             <div class="row">
                 <div class="col-12 grid-margin stretch-card">
