283483cc7a3d3bfc3f149debe7f7867fb312449d
moses848484 <moses.blake.simataa@gmail.com>
2025-08-08 17:37:39 +0200

    scsc

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -26,7 +26,50 @@
       href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+   <!-- Material Design Icons -->
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/vendors/mdi/css/materialdesignicons.min.css" />
 
+   <!-- FontAwesome (choose one version only) -->
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/css/fontawesome-free-6.6.0-web/css/all.min.css" />
+
+   <!-- Plugin CSS -->
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/vendors/jvectormap/jquery-jvectormap.css" />
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/vendors/flag-icon-css/css/flag-icon.min.css" />
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.carousel.min.css" />
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/admin/assets/vendors/owl-carousel-2/owl.theme.default.min.css" />
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet"
+      href="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+
+   <!-- Your custom styles -->
+   <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style.css" />
+   <link rel="stylesheet" href="https://seventh-production.up.railway.app/admin/assets/css/style1.css" />
+
+   <!-- Favicon -->
+   <link rel="shortcut icon" href="https://seventh-production.up.railway.app/admin/assets/images/sda3.png" />
+
+   <!-- Your custom script -->
+   <script src="https://seventh-production.up.railway.app/images/script.js" defer></script>
+   <script src="/images/script.js"></script>
+
+
+   <!-- Bootstrap 4 CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css">
+
+
+
+   <!-- Bootstrap 5 CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+   <!-- Bootstrap 5 JS (includes Popper) -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
    @notifyCss
 </head>
 
@@ -61,6 +104,17 @@
          <script src="https://seventh-production.up.railway.app/home/js/bootstrap.js"></script>
          <!-- custom js -->
          <script src="https://seventh-production.up.railway.app/home/js/custom.js"></script>
+         <!-- jQuery (required by Bootstrap 4) -->
+         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+         <!-- Popper.js -->
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+         <!-- Bootstrap 4 JS -->
+         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"></script>
+         <!-- Bootstrap JS Bundle (includes Popper) -->
+         <script src="https://seventh-production.up.railway.app/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+            defer></script>
          <x-notify::notify />
          @notifyJs
       </div>
