f142b03dd3063df1b5acff44ca089a9a9ce080fe
moses848484 <moses.blake.simataa@gmail.com>
2025-08-09 03:36:38 +0200

    dcdc

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -232,7 +232,7 @@
     <!-- Leaflet JS -->
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
     <script>
         let map;
