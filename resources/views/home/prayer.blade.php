639b6f393ff5d3287affbf640a7eb717d7208f9f
moses848484 <moses.blake.simataa@gmail.com>
2025-08-09 15:08:49 +0200

    dfdf

--- resources/views/home/prayer.blade.php
+++ resources/views/home/prayer.blade.php
@@ -1,41 +1,34 @@
 <style>
-.video-embed {
-  max-width: 100%;
-  /* Ensures the image does not exceed the container's width */
-  height: 100%;
-  /* Maintains the aspect ratio */
-  width: 100%;
-  /* Ensures the image scales to fill its container */
-  display: block;
-  /* Removes any default inline spacing or gaps */
-}
-
+    .video-embed {
+        width: 500px;
+        height: 500px;
+        min-height: 500px;
+    }
 </style>
 <section data-testid="dualcontentzone" class="section1">
     <div class="row align-items-center">
-             <!-- Video Column -->
-                <div class="col-md-6">
-                    <div class="arrival_bg_box">
-                        <iframe 
-                            src="https://www.youtube.com/embed/qzp33k3wJvY?autoplay=0&controls=1&rel=0&showinfo=0&modestbranding=1" 
-                            title="University SDA Church Welcome Video" 
-                            frameborder="0" 
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                            allowfullscreen
-                            class="video-embed">
-                        </iframe>
-                    </div>
-                </div>
+        <!-- Video Column -->
+        <div class="col-md-6">
+            <div class="arrival_bg_box">
+                <iframe
+                    src="https://www.youtube.com/embed/qzp33k3wJvY?autoplay=0&controls=1&rel=0&showinfo=0&modestbranding=1"
+                    title="University SDA Church Welcome Video" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen class="video-embed">
+                </iframe>
+            </div>
+        </div>
         <!-- Text Column -->
         <div class="col-md-6">
             <div class="text-area7 bg text-left text-black">
                 <div class="spacer-wrapper pt-very_relaxed"></div>
                 <h4 class="heading3 text-section_header3 mb-relaxed">
-                You’re Welcome Here!
+                    You’re Welcome Here!
                 </h4>
                 <div class="rich-text3 text-paragraph_large mb-relaxed" data-testid="lc-rich-text-component">
                     <p>
-                        University SDA.Church is one church meeting in multiple locations, and we want to help you become the
+                        University SDA.Church is one church meeting in multiple locations, and we want to help you
+                        become the
                         person God made you to be. No matter where you are in your journey, you’re invited to discover
                         your purpose and live it out at University SDA.Church.
                     </p>
@@ -43,10 +36,10 @@
 
                 <div class="btn-box">
                     <a href="{{ route('register') }}" class="btn3">
-                       What to Expect
+                        What to Expect
                     </a>
                     <a href="{{ route('register') }}" class="btn4">
-                    <i class="fa-solid fa-location-dot"></i>&nbsp;Find a Location
+                        <i class="fa-solid fa-location-dot"></i>&nbsp;Find a Location
                     </a>
                 </div>
                 <div class="spacer-wrapper pt-normal"></div>
