4eef2d65afb7c464f7b9fd1d17473c93d9f2b34c
moses848484 <moses.blake.simataa@gmail.com>
2025-08-09 14:17:40 +0200

    sdsd

--- resources/views/home/prayer.blade.php
+++ resources/views/home/prayer.blade.php
@@ -2,20 +2,23 @@
     <div class="row align-items-center">
         <!-- Image Column -->
         <div class="col-md-6">
-            <div class="arrival_bg_box">
-                <img src="images/happy-girl.jpg" alt="Person praying" class="img-fluid">
+            <div class="arrival_bg_box ratio ratio-16x9">
+                <iframe src="https://www.youtube.com/embed/qzp33k3wJvY" title="YouTube video player" allowfullscreen>
+                </iframe>
             </div>
         </div>
+
         <!-- Text Column -->
         <div class="col-md-6">
             <div class="text-area7 bg text-left text-black">
                 <div class="spacer-wrapper pt-very_relaxed"></div>
                 <h4 class="heading3 text-section_header3 mb-relaxed">
-                You’re Welcome Here!
+                    You’re Welcome Here!
                 </h4>
                 <div class="rich-text3 text-paragraph_large mb-relaxed" data-testid="lc-rich-text-component">
                     <p>
-                        University SDA.Church is one church meeting in multiple locations, and we want to help you become the
+                        University SDA.Church is one church meeting in multiple locations, and we want to help you
+                        become the
                         person God made you to be. No matter where you are in your journey, you’re invited to discover
                         your purpose and live it out at University SDA.Church.
                     </p>
@@ -23,10 +26,10 @@
 
                 <div class="btn-box">
                     <a href="{{ route('register') }}" class="btn3">
-                       What to Expect
+                        What to Expect
                     </a>
                     <a href="{{ route('register') }}" class="btn4">
-                    <i class="fa-solid fa-location-dot"></i>&nbsp;Find a Location
+                        <i class="fa-solid fa-location-dot"></i>&nbsp;Find a Location
                     </a>
                 </div>
                 <div class="spacer-wrapper pt-normal"></div>
