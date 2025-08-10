078d5199bfc9c70931529e9181adf5b1b14cd4b8
moses848484 <moses.blake.simataa@gmail.com>
2025-08-10 20:22:12 +0200

    dfdf

--- resources/views/home/what-to-expect.blade.php
+++ resources/views/home/what-to-expect.blade.php
@@ -8,15 +8,7 @@
     <link rel="shortcut icon" href="https://seventh-production.up.railway.app/images/sda3.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <title>What to Expect - SDA Church</title>
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/style.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/responsive.css" />
-    <link rel="stylesheet"
-        href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
-
-    <!-- Bootstrap 4 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 
     <style>
         body {
@@ -45,6 +37,7 @@
             display: flex;
             flex-direction: column;
             height: 100%;
+            margin-bottom: 1.5rem; /* Added margin-bottom here for spacing */
         }
 
         .expectation-card:hover {
@@ -78,11 +71,27 @@
         .lead {
             color: white;
         }
+
+        /* Optional smaller adjustments on mobile */
+        @media (max-width: 576px) {
+            .expectation-card {
+                padding: 20px;
+            }
+            .expectation-icon {
+                width: 50px;
+                height: 50px;
+                font-size: 20px;
+            }
+            h4 {
+                font-size: 1.25rem;
+            }
+        }
     </style>
 </head>
 
 <body>
     @include('home.header')
+
     <!-- Hero Section -->
     <section class="hero-section text-center">
         <div class="container">
@@ -96,7 +105,7 @@
         <div class="container">
             <div class="row justify-content-center text-center">
                 <!-- Card 1 -->
-                <div class="col-md-4 mb-4 d-flex">
+                <div class="col-12 col-sm-6 col-md-4 d-flex">
                     <div class="expectation-card w-100">
                         <div class="expectation-icon">
                             <i class="fas fa-clock"></i>
@@ -110,27 +119,25 @@
                 </div>
 
                 <!-- Card 2 -->
-                <div class="col-md-4 mb-4 d-flex">
+                <div class="col-12 col-sm-6 col-md-4 d-flex">
                     <div class="expectation-card w-100">
                         <div class="expectation-icon">
                             <i class="fas fa-users"></i>
                         </div>
                         <h4>Friendly Atmosphere</h4>
-                        <p>Our greeters will welcome you at the door and help you find your way. Our
-                            community is warm and welcoming!</p>
+                        <p>Our greeters will welcome you at the door and help you find your way. Our community is warm and welcoming!</p>
                         <small>Feel free to introduce yourself</small>
                     </div>
                 </div>
 
                 <!-- Card 3 -->
-                <div class="col-md-4 mb-4 d-flex">
+                <div class="col-12 col-sm-6 col-md-4 d-flex">
                     <div class="expectation-card w-100">
                         <div class="expectation-icon">
                             <i class="fas fa-tshirt"></i>
                         </div>
                         <h4>Come As You Are</h4>
-                        <p>Dress comfortably! You'll see everything from casual to business attire. What
-                            matters most is that you're here.</p>
+                        <p>Dress comfortably! You'll see everything from casual to business attire. What matters most is that you're here.</p>
                         <small>No dress code required</small>
                     </div>
                 </div>
@@ -142,7 +149,6 @@
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 
 </html>
