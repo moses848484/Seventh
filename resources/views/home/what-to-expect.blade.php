31c9ad77ac022ef79558c0b67ee438ecf3268e59
moses848484 <moses.blake.simataa@gmail.com>
2025-08-11 00:30:19 +0200

    gfgf

--- resources/views/home/what-to-expect.blade.php
+++ resources/views/home/what-to-expect.blade.php
@@ -100,7 +100,7 @@
         .image-slider-container {
             position: relative;
             width: 100%;
-            height: 300px;
+            height: 800px;
         }
 
         .image-slider-wrapper {
