42b396de564c673b45dce222213cf312f58f7a05
moses848484 <moses.blake.simataa@gmail.com>
2025-08-10 22:50:57 +0200

    sdsd

--- resources/views/home/what-to-expect.blade.php
+++ resources/views/home/what-to-expect.blade.php
@@ -100,7 +100,7 @@
         .image-slider-container {
             position: relative;
             width: 100%;
-            height: 200px;
+            height: 300px;
         }
 
         .image-slider-wrapper {
