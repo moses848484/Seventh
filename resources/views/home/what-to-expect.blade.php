d0437e2578a0197a5ba1d86a5c8a4ec8d16abf27
moses848484 <moses.blake.simataa@gmail.com>
2025-08-11 23:54:40 +0200

    erer

--- resources/views/home/what-to-expect.blade.php
+++ resources/views/home/what-to-expect.blade.php
@@ -357,21 +357,13 @@
         <div class="text-area12 bg text-left text-black">
             <div class="spacer-wrapper pt-very_relaxed"></div>
             <h4 class="heading3 text-section_header mb-relaxed">
-                Need A Prayer?
+                Everyone’s Invited
             </h4>
             <div class="rich-text3 text-paragraph_large mb-relaxed" data-testid="lc-rich-text-component">
                 <p>
-                    There are times when life is overwhelming, and all we have are questions. In those moments,
-                    hope can feel far away. The great thing about prayer is that it shifts our perspective
-                    toward the One who stands ready to listen. No matter what you’re facing, we’d love to pray
-                    with you!
+                    UNISDA Church started with a simple idea: What if there were a church where you could just come as you
+                    are? Wherever you are on your journey, there’s a place for you here.
                 </p>
-            </div>
-
-            <div class="btn-box3">
-                <a href="{{ route('register') }}" class="btn4">
-                    <i class="fa-solid fa-hands-praying"></i>&nbsp;Ask for prayer
-                </a>
             </div>
         </div>
         <div class="spacer-wrapper pt-normal"></div>
