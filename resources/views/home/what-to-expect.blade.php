ca9b6463bf1f143cb58ab22ca1a97bc9487d8c18
moses848484 <moses.blake.simataa@gmail.com>
2025-08-11 15:41:25 +0200

    sdsd

--- resources/views/home/what-to-expect.blade.php
+++ resources/views/home/what-to-expect.blade.php
@@ -5,16 +5,10 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="shortcut icon" href="https://seventh-production.up.railway.app/images/sda3.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <title>What to Expect - SDA Church</title>
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/style.css" />
-    <link rel="stylesheet" href="https://seventh-production.up.railway.app/home/css/responsive.css" />
-    <link rel="stylesheet"
-        href="https://seventh-production.up.railway.app/css/fontawesome-free-6.5.2-web/css/all.min.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 
     <style>
         body {
