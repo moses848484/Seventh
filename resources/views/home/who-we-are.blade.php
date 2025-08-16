e66fb5e396dd568ad2d7e3f6a33809dcbfd4a127
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-16 11:52:25 +0200

    rt

--- resources/views/home/who-we-are.blade.php
+++ resources/views/home/who-we-are.blade.php
@@ -509,9 +509,7 @@
         </div>
         <div class="spacer-wrapper pt-normal"></div>
     </div>
-
-@include('home.slider')
-
+    
     <!-- Leadership Section -->
     <section class="py-5">
         <div class="container">
