9e7025aca1a7a356441dc0f9901bd0dc8a6ef63d
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-16 09:12:13 +0200

    dfdf

--- resources/views/home/who-we-are.blade.php
+++ resources/views/home/who-we-are.blade.php
@@ -177,7 +177,7 @@
             flex-direction: row;
             justify-content: center;
             align-items: flex-start;
-            gap: 10px;
+            gap: 1rem;
             max-width: 900px;
             margin: 0 auto;
             padding: 0 2rem;
