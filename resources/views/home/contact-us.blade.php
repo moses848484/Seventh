1715748aa9280c3c59ff94bba67c31c39f148381
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-17 09:13:44 +0200

    ef

--- resources/views/home/contact-us.blade.php
+++ resources/views/home/contact-us.blade.php
@@ -450,16 +450,16 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link second-nav" href="#">Who We Are</a>
+                        <a class="nav-link second-nav" href="{{ route('who-we-are') }}">Who We Are</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link second-nav" href="#">What to Expect</a>
+                        <a class="nav-link second-nav active" href="{{ route('what-to-expect') }}">What to Expect</a>
+                    </li>
+                     <li class="nav-item">
+                        <a class="nav-link second-nav active" href="{{ route('contact-us') }}">Contact Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link second-nav active" href="#">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link second-nav" href="#">Our Beliefs</a>
+                        <a class="nav-link second-nav" href="{{ route('our-beliefs') }}">Our Beliefs</a>
                     </li>
                 </ul>
             </div>
