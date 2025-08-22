036a399e329a977d735f8fffd4ec7a1e2c96f995
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-22 12:35:00 +0200

    df

--- resources/views/home/contact-us.blade.php
+++ resources/views/home/contact-us.blade.php
@@ -570,7 +570,7 @@
                     <div class="demo-card">
                         <div class="demo-card-icon light">
                             <img src="images/praying_hands.png" alt="Service Icon" class="service-icon"
-                                style="width:50px; height:50px; filter: brightness(0) saturate(100%) invert(92%) sepia(66%) saturate(6423%) hue-rotate(1deg) brightness(100%) contrast(102%);">
+                                style="width:50px; height:50px; filter: #e4af00;">
                         </div>
                         <div class="demo-card-content">
                             <h4>Prayer Needs</h4>
