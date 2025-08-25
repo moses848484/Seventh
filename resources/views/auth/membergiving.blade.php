d630660b38da3e15b07df045be710f33e7321b11
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-25 07:39:20 +0200

    sd

--- resources/views/auth/membergiving.blade.php
+++ resources/views/auth/membergiving.blade.php
@@ -297,7 +297,8 @@
 
         </form>
     </div>
-
+   </div>
+    <!-- Footer -->
     <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted1 d-block text-center text-sm-left d-sm-inline-block">Copyright ©
