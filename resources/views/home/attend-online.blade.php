e13dcb9f44ff35b52763b8269850ccdba4975f8f
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-25 11:43:03 +0200

    er

--- resources/views/home/attend-online.blade.php
+++ resources/views/home/attend-online.blade.php
@@ -501,7 +501,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-custom">
         <div class="container">
-            <a class="navbar-brand" href="#">About Us</a>
+            <a class="navbar-brand" href="#">Streaming Online</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="custom-toggler-icon"></span>
