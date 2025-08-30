9ca1470b5c8dae3ad0b3aaae0a330fdff295cabb
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-30 21:57:24 +0200

    sc

--- resources/views/home/body.blade.php
+++ resources/views/home/body.blade.php
@@ -255,14 +255,14 @@
 
         /* Footer styling */
         .footer1 {
-            background: #f8f9fa;
+            background: #04AA6D;
             padding: 15px;
             border-top: 1px solid #dee2e6;
             border-radius: 0 0 12px 12px;
         }
 
         .text-muted2 {
-            color: #6c757d !important;
+            color: #ffffffff !important;
             font-size: 0.9rem;
         }
 
@@ -581,21 +581,7 @@
             </div>
         </div>
 
-        <!-- Footer -->
-        <footer class="footer">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                <span class="text-muted1 d-block text-center text-sm-left d-sm-inline-block">Copyright ©
-                    University SDA Church 2024</span>
-                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-white">
-                    Computer Science Dept
-                    <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank"
-                        class="text-white">
-                        Computer Systems Engineering
-                    </a> from University Of Zambia
-                </span>
-            </div>
-        </footer>
-    </div>
+      @include('home.footer')
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
