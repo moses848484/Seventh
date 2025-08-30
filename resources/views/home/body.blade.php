60e25bc45f7c9d5f23d8d266b7feb7c228a44b67
moses848484 <121771369+moses848484@users.noreply.github.com>
2025-08-30 22:15:39 +0200

    wd

--- resources/views/home/body.blade.php
+++ resources/views/home/body.blade.php
@@ -572,7 +572,27 @@
             </div>
         </div>
 
-      @include('home.footer')
+       <!-- partial:partials/_footer.html -->
+            <footer class="footer">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted1 d-block text-center text-sm-left d-sm-inline-block">Copyright ©
+                        University
+                        SDA Church 2024</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center text-white">
+                        Computer Science Dept
+                        <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank"
+                            class="text-white">
+                            Computer Systems Engineering
+                        </a> from University Of Zambia
+                    </span>
+                </div>
+            </footer>
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+    </div>
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
